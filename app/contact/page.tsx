a83bf3ba9a002dbe9a3b93c54f7fa087e2d4e861
Sanjay Kumar <sanjayvkumar31@gmail.com>
2025-09-13 15:32:29 -0400

    reorder homepage

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,23 +6,23 @@ export default function Contact() {
         <Page pageTitle="Contact Me">
 
             <div className="grid grid-cols-1
-                            md:grid md:grid-cols-2">
+                            md:grid-cols-2">
                 <div className="col-start-1 col-span-1
-                                md:col-start-1 md:col-span-1 md:aspect-[16/5]">
+                                md:col-start-1 md:col-span-1 md:aspect-[16/6]">
                     <ProjectCard title="Send me an email" href="mailto:svk2121@columbia.edu">
                         <b>svk2121@columbia.edu</b>
                     </ProjectCard>
                 </div>
 
                 <div className="col-start-1 col-span-1
-                                md:col-start-1 md:col-span-1 md:aspect-[16/5]">
+                                md:col-start-2 md:col-span-1 md:aspect-[16/6]">
                     <ProjectCard title="Connect with me" href="https://www.linkedin.com/in/sanjayvkumar31/">
                         <b>LinkedIn/sanjayvkumar31</b>
                     </ProjectCard>
                 </div> 
 
                 <div className="col-start-1 col-span-1
-                                md:col-start-1 md:col-span-1 md:aspect-[16/5]">
+                                md:col-start-1 md:col-span-2 md:aspect-[16/3]">
                     <ProjectCard title="Check out my code" href="https://github.com/sanjito31/">
                         <b>Github/sanjito31</b>
                     </ProjectCard>
